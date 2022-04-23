55aff1bc15bdb54131320100130d899e7053655d
SlavaVG <shlikhten@gmail.com>
2022-04-23 18:36:43 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,34 +1,3 @@
-// {
-    // Колбэк-функция
-//     function greet(name) {
-//         console.log(`Добро пожаловать ${name}.`);
-//     }
-
-//     // Функция высшего порядка
-//     function registerGuest(name, callback) {
-//         console.log(`Регистрируем гостя ${name}.`);
-//         callback(name);
-//     }
-
-//     registerGuest("Mango", greet);
-// }
-
-
-    {
-        function deliverPizza(pizzaName) {
-            return `Delivering ${pizzaName} pizza.`;
-        }
-
-        function makePizza(pizzaName) {
-            return `Pizza ${pizzaName} is being prepared, please wait...`;
-        }
-
-        // Chande code below this line
-    function makeMessage(pizzaName, callback) {
-        callback(pizzaName);
-        return callback(pizzaName);
-        }
-
-    makeMessage("Royal Grand", makePizza);
-    makeMessage("Ultracheese", deliverPizza);
-    }
+const add = (a = 0, b = 10) => a + b;
+const sum = add(10);
+console.log(sum);
